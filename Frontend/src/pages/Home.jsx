eeb8302fe48cd599417f947820910fabf459d24f
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-11-23 14:50:28 -0300

    added 2 basic pages

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+// core components
+import PageHeader from "../old/components/Sections/PageHeader.jsx";
+
+import MiTrabajo from "../old/components/Sections/MiTrabajo.jsx"
+import Tecnologias from "../old/components/Sections/Tecnologias.jsx"
+import SectionContacto from "../old/components/Sections/SectionContacto.jsx"
+import PaquetesBasicos from "../old/components/Sections/PaquetesBasicos.jsx"
+
+class Index extends React.Component {
+  componentDidMount() {
+    document.body.classList.toggle("index-page");
+  }
+  componentWillUnmount() {
+    document.body.classList.toggle("index-page");
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <PageHeader />
+
+        <section className="section section-basic wrapper justify-content-between">
+          <img alt="..." className="path" src={require("../assets/img/path1.png")} />
+
+          <section className="section section-lg">
+            <MiTrabajo />
+          </section>
+
+          <section className="section section-lg">
+
+            <img alt="..." className="path" style={{ transform: 'scale(0.7)', left: '-15em', top: "-7em" }} src={require("../assets/img/path2.png")} />
+            <Tecnologias />
+          </section>
+
+        </section>
+
+        <img alt="..." className="path" src={require("../assets/img/path4.png")} />
+
+        <PaquetesBasicos />
+
+        <section className="section section-lg justify-content-center">
+          <SectionContacto />
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index;
