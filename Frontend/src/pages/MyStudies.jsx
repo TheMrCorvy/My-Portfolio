e7b03e747512ef97725f414c0d1a97c0ee8759c0
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-12-16 19:07:16 -0300

    added a title and description to the page

--- Frontend/src/pages/MyStudies.jsx
+++ Frontend/src/pages/MyStudies.jsx
@@ -16,42 +16,55 @@ const MyStudies = () => {
 		setCertificates(projects)
 	}, [])
 	return (
-		<Container
-			className="bg-default"
-			style={{ marginTop: "7rem", borderRadius: 15, maxWidth: "90%", overflowX: "hidden" }}
-		>
-			<VerticalTimeline>
-				{certificates.map((certificate, index) => (
+		<>
+			<Container style={{ marginTop: "7rem" }} className="text-center mb-4">
+				<h2 className="title pb-0 mb-2">My Studies</h2>
+				<p>
+					This is the complete timeline listing all of my studies, be it with or without a
+					certification.
+				</p>
+			</Container>
+			<Container
+				className="bg-default"
+				style={{
+					borderRadius: 15,
+					maxWidth: "90%",
+					overflowX: "hidden",
+				}}
+			>
+				<VerticalTimeline>
+					{certificates.map((certificate, index) => (
+						<VerticalTimelineElement
+							key={index}
+							className="vertical-timeline-element--work"
+							contentStyle={{ background: "rgb(33, 150, 243)" }}
+							contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+							date={certificate.date}
+							dateClassName="text-white"
+							iconClassName="timeline-icon"
+							iconStyle={{
+								background: "rgb(33, 150, 243)",
+								color: "#fff",
+							}}
+							icon={<FontAwesomeIcon icon={["fas", "user-graduate"]} size="2x" />}
+						>
+							<h3 className="vertical-timeline-element-title">{certificate.name}</h3>
+							<h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+							<p dangerouslySetInnerHTML={{ __html: certificate.description }}></p>
+							<CardImg
+								alt="alt..."
+								src={certificate.image}
+								style={{ marginTop: "2rem", borderRadius: 5 }}
+							/>
+						</VerticalTimelineElement>
+					))}
 					<VerticalTimelineElement
-						key={index}
-						className="vertical-timeline-element--work"
-						contentStyle={{ background: "rgb(33, 150, 243)" }}
-						contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-						date={certificate.date}
-						dateClassName="text-white"
-						iconClassName="timeline-icon"
-						iconStyle={{
-							background: "rgb(33, 150, 243)",
-							color: "#fff",
-						}}
-						icon={<FontAwesomeIcon icon={["fas", "user-graduate"]} size="2x" />}
-					>
-						<h3 className="vertical-timeline-element-title">{certificate.name}</h3>
-						<h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-						<p dangerouslySetInnerHTML={{ __html: certificate.description }}></p>
-						<CardImg
-							alt="alt..."
-							src={certificate.image}
-							style={{ marginTop: "2rem", borderRadius: 5 }}
-						/>
-					</VerticalTimelineElement>
-				))}
-				<VerticalTimelineElement
-					iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-					icon={<FontAwesomeIcon icon={["fas", "flag"]} size="2x" />}
-				/>
-			</VerticalTimeline>
-		</Container>
+						iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+						icon={<FontAwesomeIcon icon={["fas", "flag"]} size="2x" />}
+					/>
+				</VerticalTimeline>
+			</Container>
+		</>
 	)
 }
 
