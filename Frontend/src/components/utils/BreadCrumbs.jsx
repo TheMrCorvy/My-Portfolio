42d51510991b5b357cfa7443e2ed7e83ba530d2c
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-12-20 13:53:48 -0300

    added "go back" btn

--- Frontend/src/components/utils/BreadCrumbs.jsx
+++ Frontend/src/components/utils/BreadCrumbs.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 
 import { Link } from "react-router-dom"
 
-import { Breadcrumb, BreadcrumbItem, Container } from "reactstrap"
+import { Breadcrumb, BreadcrumbItem, Button, Container } from "reactstrap"
 
 const BreadCrumbs = ({ title, steps }) => {
 	const [urlSteps, setUrlSteps] = useState([""])
@@ -16,17 +16,46 @@ const BreadCrumbs = ({ title, steps }) => {
 	return (
 		<Container className="mt-5 pt-5">
 			<Breadcrumb>
-				<BreadcrumbItem>
+				<BreadcrumbItem
+					style={{
+						display: "flex",
+						alignItems: "center",
+						verticalAlign: "center",
+						textAlign: "center",
+					}}
+				>
 					<Link to="/">Home Page</Link>
 				</BreadcrumbItem>
 
 				{urlSteps.map((step, index) => (
-					<BreadcrumbItem key={index}>
+					<BreadcrumbItem
+						key={index}
+						style={{
+							display: "flex",
+							alignItems: "center",
+							verticalAlign: "center",
+							textAlign: "center",
+						}}
+					>
 						<Link to={"/" + step[1]}>{step[0]}</Link>
 					</BreadcrumbItem>
 				))}
 
-				<BreadcrumbItem active>{title}</BreadcrumbItem>
+				<BreadcrumbItem
+					active
+					style={{
+						display: "flex",
+						alignItems: "center",
+						verticalAlign: "center",
+						textAlign: "center",
+					}}
+				>
+					{title}
+				</BreadcrumbItem>
+				<div style={{ flexGrow: 1 }}></div>
+				<Button color="info" size="sm" onClick={() => window.history.go(-1)}>
+					Back
+				</Button>
 			</Breadcrumb>
 		</Container>
 	)
