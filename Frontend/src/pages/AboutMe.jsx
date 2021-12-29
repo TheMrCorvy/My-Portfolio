f02e1c509baf6a530ee4d832a328fc49facf996e
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-12-29 09:19:44 -0300

    created yugioh section for about me page

--- Frontend/src/pages/AboutMe.jsx
+++ Frontend/src/pages/AboutMe.jsx
@@ -1,10 +1,27 @@
 import React from "react"
+
+import { Row, Col, Container } from "reactstrap"
+
 import MyDescrption from "../components/sections/MyDescription"
+import Yugioh from "../components/sections/Yugioh"
 
 const AboutMe = () => {
 	return (
 		<>
 			<MyDescrption />
+
+			<section>
+				<Container style={{ marginTop: "10vw" }}>
+					<Row className="justify-content-around">
+						<Col xs="12">
+							<h1 className="ttle">My Hobbies</h1>
+							<hr className="line-info" />
+						</Col>
+
+						<Yugioh />
+					</Row>
+				</Container>
+			</section>
 		</>
 	)
 }
