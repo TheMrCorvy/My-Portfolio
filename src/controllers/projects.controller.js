d6cff621b5df60891d166990be7268b060b2811d
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-04 16:10:29 -0300

    added basic controller

---
+++ src/controllers/projects.controller.js
@@ -0,0 +1,11 @@
+export const createProject = (req, res) => {}
+
+export const getProjects = (req, res) => {}
+
+export const getLastProject = (req, res) => {
+	return res.json("last project")
+}
+
+export const updateProject = (req, res) => {}
+
+export const deleteProject = (req, res) => {}
