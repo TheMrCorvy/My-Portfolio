d50166cb14fd8b47a34320f6fb5911f09f090125
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-05 08:41:45 -0300

    fixed "req.body is undefined"

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express"
 import morgan from "morgan"
+import bodyParser from "body-parser"
 
 import projectRoutes from "./routes/projects.routes.js"
 
 const app = express()
 
+// create application/json parser
+const jsonParser = bodyParser.json()
+// https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+
 app.use(morgan("dev"))
 
-app.use("/projects", projectRoutes)
+app.use("/projects", jsonParser, projectRoutes)
 
 export default app
