eb77037ad4d9debf33013ba7952ab846386131d7
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-05 11:27:58 -0300

    added basic reagister func

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,17 @@
 import User from "../models/User"
 
-export const register = async (req, res) => {}
+export const register = async (req, res) => {
+	const { email, password } = req.body
+
+	const userCreated = new User({
+		email,
+		password: User.encryptPassword(password),
+	})
+
+	await userCreated.save()
+
+	return res.status(200).json(userCreated)
+}
 
 export const login = async (req, res) => {}
 
