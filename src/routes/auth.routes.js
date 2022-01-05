4e638e5dc3ab85a07c6997d2940f491b33e71af3
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-05 10:31:48 -0300

    added basic auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express"
 
+import * as authController from "../controllers/auth.controller"
+
 const router = Router()
+
+router.post("/register", authController.register)
+
+router.post("/login", authController.login)
+
+router.get("/logout", authController.logout)
 
 export default router
