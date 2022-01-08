8cc1be09e9bd2044a4aea1adc2c3417aaa7e9787
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-08 08:02:04 -0300

    added the rest of the models

---
+++ src/models/Certificate.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose"
+
+const certificateSchema = new Schema(
+	{
+		name: String,
+		image: String,
+		date: String,
+		description: String,
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	}
+)
+
+export default model("Certificate", certificateSchema)
