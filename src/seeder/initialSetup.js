633713387e54ae36b246709f075d15a61eebff39
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-10 10:13:24 -0300

    formatted the console.logs

--- src/seeder/initialSetup.js
+++ src/seeder/initialSetup.js
@@ -5,6 +5,7 @@ import Project from "../models/Project"
 import World from "../models/World"
 
 import animes from "./animes.json"
+import certificates from "./certificates.json"
 import userJson from "./users.json"
 
 export const createUsers = async () => {
@@ -14,18 +15,41 @@ export const createUsers = async () => {
 
 	await Anime.deleteMany({})
 
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the animes.")
+
 	await Certificate.deleteMany({})
+
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the certificates.")
 
 	await Project.deleteMany({})
 
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the projects.")
+
 	await World.deleteMany({})
+
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Deleted all of the worlds.")
 
 	await new User({
 		email: userJson.user.email,
 		password: await User.encryptPassword(userJson.user.password),
 	}).save()
 
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Inserted user.")
+
 	await Anime.insertMany(animes.data)
+
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Inserted animes.")
+
+	await Certificate.insertMany(certificates.data)
+
+	console.log("- - - - - - - - - - - - - - -")
+	console.log("Inserted certificates.")
 
 	console.log("Database seded successfully.")
 }
