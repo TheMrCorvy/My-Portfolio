b4b735a8fe42ba9bcfc1281950a2a55381f38cb5
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-11 07:32:53 -0300

    fixed warnings

--- Frontend/src/components/sections/about-me/MyDescription.jsx
+++ Frontend/src/components/sections/about-me/MyDescription.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Container, Row, Col, UncontrolledTooltip } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 
 const MyDescrption = () => {
 	return (
