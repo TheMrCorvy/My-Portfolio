10ede369a5123e0c87db0a7a6ec78d4475efb0dd
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-11 09:11:26 -0300

    updated endpoints

--- src/controllers/certificates.controller.js
+++ src/controllers/certificates.controller.js
@@ -2,8 +2,14 @@ import Certificate from "../models/Certificate"
 
 export const create = async (req, res) => {}
 
+export const get = async (req, res) => {
+	const certificates = await Certificate.find().sort({ _id: -1 })
+
+	return res.status(200).json(certificates)
+}
+
 export const getLast = async (req, res) => {
-	const certificates = await Certificate.find().sort({ createdAt: -1 }).limit(1)
+	const certificates = await Certificate.find().sort({ _id: -1 }).limit(1)
 
 	return res.status(200).json(certificates[0])
 }
