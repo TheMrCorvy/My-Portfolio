b0ab7bd85082c970352a4121959e00c929151012
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-11 07:31:42 -0300

    I forgot to add projects to the seeder

--- src/seeder/initialSetup.js
+++ src/seeder/initialSetup.js
@@ -7,6 +7,7 @@ import World from "../models/World"
 import animes from "./animes.json"
 import certificates from "./certificates.json"
 import worlds from "./worlds.json"
+import projects from "./projects.json"
 import userJson from "./users.json"
 
 export const createUsers = async () => {
@@ -18,44 +19,50 @@ export const createUsers = async () => {
 
 	console.log("- - - - - - - - - - - - - - -")
 	console.log("Deleted all of the animes.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await Certificate.deleteMany({})
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Deleted all of the certificates.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await Project.deleteMany({})
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Deleted all of the projects.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await World.deleteMany({})
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Deleted all of the worlds.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await new User({
 		email: userJson.user.email,
 		password: await User.encryptPassword(userJson.user.password),
 	}).save()
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Inserted user.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await Anime.insertMany(animes.data)
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Inserted animes.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await Certificate.insertMany(certificates.data)
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Inserted certificates.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	await World.insertMany(worlds.data)
 
-	console.log("- - - - - - - - - - - - - - -")
 	console.log("Inserted worlds.")
+	console.log("- - - - - - - - - - - - - - -")
+
+	await Project.insertMany(projects.data)
+
+	console.log("Inserted projects.")
+	console.log("- - - - - - - - - - - - - - -")
 
 	console.log("Database seded successfully.")
 }
