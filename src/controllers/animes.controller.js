553d6fae2b32acd4afe3fa5a0ffa756aab42932f
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-11 11:24:21 -0300

    fixed position bug

--- src/controllers/animes.controller.js
+++ src/controllers/animes.controller.js
@@ -1,11 +1,22 @@
 import Anime from "../models/Anime"
 
 export const create = async (req, res) => {
-	const newAnime = new Anime(req.body)
+	const { name, disk, description, date, hasEnd } = req.body
+
+	const lastAnime = await Anime.find().sort({ _id: -1 }).limit(1)
+
+	const newAnime = new Anime({
+		name,
+		position: lastAnime[0].position + 1,
+		disk,
+		description,
+		date,
+		hasEnd,
+	})
 
 	const animeCreated = await newAnime.save()
 
-	return res.status(201).json(animeCreated)
+	return res.status(201).json({ animeCreated, status: 201 })
 }
 
 export const update = async (req, res) => {
