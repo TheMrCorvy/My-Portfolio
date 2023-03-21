d633938ca1ffc5b1ccbe4f5460d7138a0ba3ea97
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2023-03-21 03:24:52 +0000

    updated uri again

--- Frontend/src/hooks/useApi.js
+++ Frontend/src/hooks/useApi.js
@@ -3,7 +3,7 @@ const useApi = async (request) => {
 
 	const local = process.env.NODE_ENV === "development"
 
-	const baseUri = local ? "http://localhost:5000" : "https://corvalangonzalo-backend.netlify.app"
+	const baseUri = local ? "http://localhost:5000" : "https://corvalangonzalo-backend.onrender.com"
 
 	const headerToken = { "x-access-token": token ? token : "" }
 
